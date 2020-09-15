bf0d66bfd0d693972dc90f43936ce64596f5c000
Marc Lucraft <marclucraft@gmail.com>
2020-09-15 21:53:55 +0100

    Added total expense summary

---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import selectExpenses from '../selectors/expenses'
+import selectExpensesTotal from '../selectors/expenses-total'
+import numeral from 'numeral'
+require('numeral/locales/en-gb')
+numeral.locale('en-gb')
+
+export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
+  const plural = expenseCount === 1 ? '' : 's'
+  const formattedTotal = numeral(expensesTotal / 100).format('$0,0.00')
+  return (
+    <div>
+      { expenseCount > 0 && <h1>Viewing {expenseCount} expense{plural} totalling {formattedTotal}</h1> }
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  const visibleExpenses = selectExpenses(state.expenses, state.filters)
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal(visibleExpenses)
+  }
+}
+
+export default connect(mapStateToProps)(ExpensesSummary)
+
+
